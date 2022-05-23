75f2e695ce8d4e291d45cabc2b422701266214e9
Tomoko-hub <haisaitomoco@gmail.com>
2022-05-23 11:04:45 +0300

    array section ready

---
+++ src/array.ts
@@ -0,0 +1,21 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [10, 20, 30];
+let cities:Array<string> = [ 'Tokyo', 'Osaka', 'Kyoto'];
+
+let strings:string[] = [ 'JavaScript', 'TypeScript', 'Node.js' ];
+let strings2:Array<string> = ['C', 'Java', 'PHP'];
+
+let challenge: number[][] = [
+    [50, 100],
+    [30, 60],
+    [20, 40]
+];
+
+let challenge2: (string | number | boolean) []= [10, false, 'Japan'];
+
+console.log(strings);
+console.log(strings2);
+console.log(challenge2);
