3be1a1d543a1963bda3265f398f6e9345b3714b7
Tomoko-hub <haisaitomoco@gmail.com>
2022-06-06 22:07:07 +0300

    learn object

---
+++ src/object.ts
@@ -0,0 +1,10 @@
+export {};
+
+let profile1: object = { name: 'Tomoko'}; 
+profile1 = { birthYear: 1978 };
+
+let profile2: {
+    name: string;
+} = { name: 'Topi' };
+
+profile2 = { name: 'Taavi' };
