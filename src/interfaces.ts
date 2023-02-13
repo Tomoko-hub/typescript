42b5a0f36f66c64cec26daebd4066a58f9a2dc49
Tomoko-hub <haisaitomoco@gmail.com>
2023-02-13 13:18:52 +0200

    i am ready to go

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjectType = {
+    name:string;
+    level:number;
+}
+
+interface ObjectInterface {
+    name: string;
+    level: number;
+}
+
+let object:ObjectInterface = {
+    name: 'Tomoko',
+    level: 5
+};
---
+++ src/type-safety.ts
@@ -0,0 +1,3 @@
+export {};
+
+let isFinnished: boolean = true;
